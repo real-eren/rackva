d8ba7e7e8b6bb65b812133c962c654a15a1c0e2d
real-eren <90657474+real-eren@users.noreply.github.com>
2022-02-14 13:34:14 -0500

    add documentation and expand param names

--- util/my-lens.rkt
+++ util/my-lens.rkt
@@ -9,7 +9,6 @@
                       
                       identity-lens
                       lens-compose
-                      lens-thrush
                       
                       car-lens
                       cdr-lens
@@ -23,6 +22,26 @@
 ;; mostly adheres to the API found at                    ;;
 ;; https://docs.racket-lang.org/lens/lens-reference.html ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; A lens, as implemented here, is a pair of a getter and a setter
+;; foo-bar-lens = (foo-bar-getter . foo-bar-setter)
+;; The setter is not a mutating function,
+;; rather, it returns a copy with the changes applied
+;; 
+;; ex:
+;; point-x-lens is a lens that takes a point as a 'target'
+;;   and produces a 'view' of the x member
+;;
+;; (lens-view point point-x-lens) returns the x member of point
+;; 
+;; (lens-set point point-x-lens 2.1) produces a copy of point with the x member == 2.1
+;; 
+;; This module allows you to
+;; * treat getter/setters as first-class values
+;; * Compose them to access deeply nested members
+;; * Re-use them across structures with identical shapes
+;;
+
 
 ;; takes a getter and a setter
 (define make-lens cons)
@@ -31,35 +50,36 @@
 ;; returns the setter of a lens
 (define setter cdr)
 
-;; applies l's getter to t
+;; applies lenz's getter to target
 (define lens-view
-  (lambda (l t)
-    ((getter l) t)))
+  (lambda (lenz target)
+    ((getter lenz) target)))
 
-;; applies l's setter to t with val
-;; note: does NOT mutate t.
-;; Returns a copy of t with val replacing the view via l
+;; applies lenz's setter to target with val
+;; note: does NOT mutate target.
+;; Returns a copy of target with val replacing the member viewed via lenz
 (define lens-set
-  (lambda (l t val)
-    ((setter l) t val)))
+  (lambda (lenz target val)
+    ((setter lenz) target val)))
 
 ;; transforms the view (via l) of t with mapping function f
+;; get, transform, set
 (define lens-transform
-  (lambda (l t f)
-    (lens-set (setter l)
-              t
-              (f (lens-view l t)))))
+  (lambda (lenz target transform)
+    (lens-set (setter lenz)
+              target
+              (transform (lens-view lenz target)))))
 
 ;; performs no destructuring
-;; (combine2 l identity-lens) -> l
-;; (combine2 identity-lens l) -> l
+;; (combine2 lenz identity-lens) -> lenz
+;; (combine2 identity-lens lenz) -> lenz
 (define identity-lens
-  (make-lens (lambda (t) t)
-             (lambda (t val) val)))
+  (make-lens (lambda (target) target)
+             (lambda (target val) val)))
 
 
 ;; first-lens accepts the view of second-lens
-;; ex: second-lens = (combine2 car-lens cdr-lens)
+;; ex: cadr-lens = (combine2 car-lens cdr-lens)
 ;; views the car of the cdr
 (define combine2
   (lambda (first-lens second-lens)
@@ -67,24 +87,23 @@
       [(eq? second-lens identity-lens)      first-lens]
       [(eq? first-lens identity-lens)      second-lens]
       [else
-       (make-lens (lambda (t)
+       (make-lens (lambda (target)
                     (lens-view first-lens
                                (lens-view second-lens
-                                          t)))
-                  (lambda (t val)
+                                          target)))
+                  (lambda (target val)
                     (lens-set second-lens
-                              t
+                              target
                               (lens-set first-lens
                                         (lens-view second-lens
-                                                   t)
+                                                   target)
                                         val))))])))
 
 ;; combines the lenses such that
 ;; 'eye' -> first lens -> ... -> last lens -> target
 ;; lens i consumes the view of lens i+1
-;; ex: lens into 3-dimensional matrix for elem at x=1,y=2,z=3
-;; where each dimension is a row. x.y.z
-;; (lens-compose '(third-lens second-lens first-lens))
+;; ex: caddr-lens  car of cdr of cdr of target
+;; (lens-compose '(car-lens cdr-lens cdr-lens))
 (define lens-compose
   (lambda (lenses)
     ; (foldr combine2 identity-lens lenses)
@@ -96,7 +115,6 @@
 
 ;; combines the lenses such that
 ;; 'eye' -> last lens -> ... -> first lens -> target
-;; lens i consumes the view of lens i-1
 ;; opposite order of lens-compose
 (define lens-thrush
   (lambda (lenses)
@@ -106,13 +124,13 @@
 
 (define car-lens
   (make-lens car
-             (lambda (p c)
-               (cons c (cdr p)))))
+             (lambda (p v)
+               (cons v (cdr p)))))
 
 (define cdr-lens
   (make-lens cdr
-             (lambda (p c)
-               (cons (car p) c))))
+             (lambda (p v)
+               (cons (car p) v))))
 
 
 (define first-lens car-lens)
@@ -126,4 +144,3 @@
     (if (= k 1)
         car-lens
         (combine2 (list-kth-elem-lens (- k 1)) cdr-lens))))
-
