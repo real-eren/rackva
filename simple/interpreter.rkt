bceec208d6ae8c8ce032f6b07d4614edad617284
real-eren <exk379@case.edu>
2022-02-15 16:57:02 -0500

    add exports and basic logic

--- simple/interpreter.rkt
+++ simple/interpreter.rkt
@@ -1,18 +1,30 @@
 #lang racket
+(require "state.rkt")
 
+(provide interpret
+         Mstate)
 
 (define interpret
   (lambda (statements)
-    (print-result)))
+    (print-result (Mstate (cdr statements)
+                          (Mstate (car statements)
+                                  new-state)))))
 
 
 (define Mstate
   (lambda (expr state)
-    (cond)))
+    (cond
+      [(state-return? state)         state] ; exit early on return
+      )))
 
 
+;; takes a state and prints the output
 (define print-result
-  (lambda (result)
-    (cond)))
-
+  (lambda (state)
+    (cond
+      [(not (state-return? state))          (error "Missing return statement")]
+      [(eq? #t (state-value state))         (print "true")]
+      [(eq? #f (state-value state))         (print "false")]
+      [(number? (state-value state))        (print (state-value state))]
+      [else                                 (error "returned a value, but not a bool or int")])))
 
