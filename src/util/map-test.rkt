681712d666d29826794b54efabfe1be66320acc5
real-eren <exk379@case.edu>
2022-02-23 22:42:36 -0500

    move map to utils

--- src/util/map-test.rkt
+++ src/util/map-test.rkt
@@ -1,10 +1,70 @@
 #lang racket
 
 (require rackunit
-         (only-in "map.rkt"
-                  map:get
-                  entry:key
-                  entry:value))
+         "map.rkt")
 
-(check-equal? (entry:key (entry:of 'a 'b))       'a)
-(check-equal? (entry:value (entry:of 'a 'b))     'b)
+;; newly constructed maps are empty
+(check-true (map-empty? map-empty))
+(check-true (map-empty? (map-empty-custom eq?)))
+
+(define map-3 (map-put 'a 1 (map-put 'b 2 (map-put 'c 3 map-empty))))
+(define map-3i (map-from-interlaced-entry-list '(a 1
+                                                 b 2
+                                                 c 3)
+                                               map-empty))
+
+; may have to replace with unordered equality
+(check-true (equal? (map-entry-list map-3)
+                    (reverse (map-entry-list map-3i))))
+
+;; contains finds most recent
+(check-true (map-contains? 'a map-3))
+;; middle entered
+(check-true (map-contains? 'b map-3))
+;; first entered
+(check-true (map-contains? 'c map-3))
+;; only 3 elements
+(check-eq? 3 (length (map-entry-list map-3)))
+;; not present
+(check-false (map-contains? 'not-in-map map-3))
+
+(check-true (map-contains? 'a (map-remove 'not-a-key map-3)))
+(check-true (map-contains? 'a (map-remove 'b map-3)))
+
+
+;;;; updating existing map
+;; entry gone after removing
+(check-false (map-contains? 'a (map-remove 'a map-3)))
+
+(check-true (map-contains? 'r (map-put 'r 123 map-3)))
+(check-eq? 4 (length (map-entry-list (map-put 'r 123 map-3))))
+
+(check-true (map-contains? 'a (map-put 'a 123 map-3)))
+
+(check-true (let ([new-v 123]
+                  [key   'a])
+              (let ([new-map (map-put key new-v map-3)])
+                (and (map-contains? key new-map)
+                     (eq? (map-get key new-map) new-v)))))
+
+;; removing each element results in an empty map
+(check-true (let ([new-map (map-remove 'c
+                                       (map-remove 'b
+                                                   (map-remove 'a map-3)))])
+              (map-empty? new-map)))
+
+;; remove-every removes multiple
+(check-false (map-contains? 'b (map-remove 'b (map-put 'b 0 map-3))))
+;; remove-every doesn't remove other
+(check-true (map-contains? 'a (map-remove 'b (map-put 'b 0 map-3))))
+
+
+;; put-if-absent behaves as claimed
+(check-true (map-contains? 'a map-3))
+(check-true (map-contains? 'a (map-put-if-absent 'a 999 map-3)))
+(check-eq? (map-get 'a map-3) (map-get 'a (map-put-if-absent 'a 999 map-3)))
+
+(check-false (map-contains? 'z map-3))
+(check-true (map-contains? 'z (map-put-if-absent 'z 999 map-3)))
+(check-eq? 999 (map-get 'z (map-put-if-absent 'z 999 map-3)))
+
--- src/util/map.rkt
+++ src/util/map.rkt
@@ -1,112 +1,167 @@
 #lang racket
 
-(require (only-in "map-entry.rkt"
-                  entry:of
-                  entry:key
-                  entry:value))
-
-(provide (prefix-out map:
+(provide (prefix-out map-
                      (combine-out empty
+                                  empty-custom
                                   empty?
+                                  entry-list
+                                  eq-fun
+                                  contains?
                                   get
-                                  result:has-value?
-                                  result:get-value
                                   get-default
-                                  insert
+                                  put
+                                  put-if-absent
                                   remove
-                                  remove-every
-                                  replace
-                                  entry:key
-                                  entry:value)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;; A list-backed map ;;;;;;;;;;;;;
-;; aka an association-list            
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                  from-interlaced-entry-list)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; A list-backed map ;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; By default, uses `equal?` to compare
+;; elements.
+;; Using the ___ constructor,
+;; you can also provide a   
+;; comparator to be used.
+;; Pair (key-comp-func and entry list)
+;;
+;; functions prefixed with _ take the entrylist of a map
 
+;;;;;;;;;;;; map entry ;;;;;;;;;;;;;;;;;;;
+(define entry-of cons)
+(define entry-key car)
+(define entry-value cdr)
 
 ;;;;;;;;;;;; map primitives ;;;;;;;;;;;;;;
-;; dependency: entry primitives
+
+;; takes an entry list and a eq-fun
+;; and returns a map
+(define construct 
+  (lambda (entry-list eq-fun) ; do not replace with cons - wrong arg order
+    (cons eq-fun entry-list)))
+
+;; returns the key equality function used by the given map
+(define eq-fun car)
+;; returns a list of the entries in the given map
+(define entry-list cdr)
+
 
 ;; creates an empty map
-(define empty '())
+(define empty (construct '() equal?))
+;; returns an empty map that will use your
+;; given equality function to compare keys
+;; your eq-fun should take two keys
+;; the first being the key being looked up
+;; and the second being the map's entries' keys
+;; return whether they are equal
+(define empty-custom
+  (lambda (eq-fun?)
+    (construct '() eq-fun?)))
 
 ;; returns whether the map is empty
-(define empty? null?)
+(define empty?
+  (lambda (map)
+    (null? (entry-list map))))
 
-;; returns some entry from the map
+
+;; returns the first entry from the given entry list
 ; could weaken to "some" or "any"
 (define first car)
-
-;; returns a map containing every entry except that returned by first
+;; returns a list containing every entry except that returned by first
 (define rest cdr)
-
 
 ;;;;;;;; map functions ;;;;;;;;
 
-;; returns, if present, the entry in the map-list with the given key
+; returns whether an entry with the given key exists
+(define contains?
+  (lambda (key map)
+    (_contains? key (entry-list map) (eq-fun map))))
+
+(define _contains?
+  (lambda (key entry-list eq-fun?)
+    (ormap (lambda (entry) (eq-fun? key (entry-key entry))) entry-list)))
+
+;; returns, if present, the value of the entry in the map-list with the given key
+;; else null
 (define get
   (lambda (key map)
-    (cond
-      [(empty? map)                               (result #f '())]
-      [(equal? key (entry:key (first map)))       (result #t (entry:value (first map)))]
-      [else                                       (get key (rest map))])))
-
-
-;; lenses for return value of lookup w/out default value
-(define result:has-value?
-  (lambda (result)
-    (car result)))
-; returns the value from lookup. undefined if has-value is false
-(define result:get-value
-  (lambda (result) ; can add check has-value for debugging purposes
-    (cdr result)))
-; constructs a result for a lookup
-(define result
-  (lambda (present? value)
-    (cons present? value)))
-
+    (get-default key null map)))
 
 ;; returns value of first encountered entry with key,
 ;; returns default if no entries found
 (define get-default
   (lambda (key default map)
+    (_get-default key
+                  default
+                  (entry-list map)
+                  (eq-fun map))))
+
+(define _get-default
+  (lambda (key default entry-list eq-fun?)
     (cond
-      [(empty? map)                                   default]
-      [(equal? key (entry:key (first map)))           (entry:value (first map))]
-      [else                                           (get-default key default (rest map))])))
+      [(null? entry-list)                                     default]
+      [(eq-fun? key (entry-key (first entry-list)))           (entry-value (first entry-list))]
+      [else                                                   (_get-default key default (rest entry-list) eq-fun?)])))
 
 
-;; inserts the key-value pair into the map
+;; adds the key-value pair to the map
 ;; does not check if an entry with the same key exists
 (define insert
   (lambda (key value map)
-    (insert-entry (entry:of key value) map)))
+    (construct (_insert key value map)
+               (eq-fun map))))
 
-;; inserts the entry into the map
-(define insert-entry
-  (lambda (entry map)
-    (cons entry map)))
+(define _insert
+  (lambda (key value entry-list)
+    (insert-entry (entry-of key value) entry-list)))
+
+;; inserts the entry into the entry list
+(define insert-entry cons)
 
 ;; removes the first entry with matching key
 (define remove
   (lambda (key map)
-    (cond
-      [(empty? map)                                    map]
-      [(equal? key (entry:key (first map)))            (rest map)]
-      [else                                            (insert-entry (first map)
-                                                               (remove key (rest map)))])))
+    (construct (_remove key (entry-list map) (eq-fun map))
+               (eq-fun map))))
 
-;; removes every entry whose key matches 
-(define remove-every
-  (lambda (key map)
+(define _remove
+  (lambda (key entry-list eq-fun?)
     (cond
-      [(empty? map)                                   map]
-      [(equal? key (entry:key (first map)))           (remove-every key (rest map))]
-      [else                                           (insert-entry (first map)
-                                                              (remove-every key (rest map)))])))
+      [(null? entry-list)                               entry-list]
+      [(eq-fun? key (entry-key (first entry-list)))     (rest entry-list)]
+      [else                                             (cons (first entry-list)
+                                                              (_remove key (rest entry-list) eq-fun?))])))
 
-;; inserts the entry, removing any entries with matching keys
-(define replace
+;; inserts the entry, removing any previous entry with a matching key
+(define put
   (lambda (key value map)
-    (insert key value (remove-every key map))))
+    (construct (_insert key
+                        value
+                        (_remove key
+                                 (entry-list map)
+                                 (eq-fun map)))
+               (eq-fun map))))
+
+;; inserts the entry iff there is not already an entry with the key
+(define put-if-absent
+  (lambda (key value map)
+    (if (contains? key map)
+        map
+        (put key value map))))
+
+
+;; takes an initial map and adds the given list to it
+;; does not check for uniqueness
+;; treats the first and second elems as key and value
+;; returns a map with the entries
+(define from-interlaced-entry-list
+  (lambda (lis map)
+    (construct (_from-interlaced-entry-list lis (entry-list map))
+               (eq-fun map))))
+
+(define _from-interlaced-entry-list
+  (lambda (lis entrylist)
+    (if (null? lis)
+        entrylist
+        (_from-interlaced-entry-list (cdr (cdr lis))
+                                     (_insert (first lis) (second lis) entrylist)))))
+
 
