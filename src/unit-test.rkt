eac2ae14c429b21ea22c1956f1812b75bc10c8a1
real-eren <exk379@case.edu>
2022-03-02 11:22:28 -0500

    add unit tests for static scoping, make test IDs descriptive

--- src/unit-test.rkt
+++ src/unit-test.rkt
@@ -33,30 +33,30 @@
 
 ; ; Number Tests
 
-(test-str #:id 1
+(test-str #:id "return a literal"
           150 "return 150;")
 
-(test-str #:id 2
+(test-str #:id "return a nested expression"
           -4 "return 6 * (8 + (5 % 3)) / 11 - 9;")
 
-(test-str #:id 3
+(test-str #:id "declare, assign a literal, return"
           10 "
 var z;
 z = 10;
 return z;")
 
-(test-str #:id 4
+(test-str #:id "declare with expression value, return"
           16 "
 var x = (5 * 7 - 3) / 2;
 return x;")
 
-(test-str #:id 5
+(test-str #:id "declare w/ literal, declare w/ reference to prior var, return sum"
           220 "
 var x = 10;
 var y = 12 + x;
 return x * y;")
 
-(test-str #:id 6
+(test-str #:id "assign in if statement, <= op"
           5 "
 var x = 5;
 var y = 6;
@@ -67,7 +67,7 @@ else
   m = y;
 return m;")
 
-(test-str #:id 7
+(test-str #:id "assign in if statement, >= op"
           6 "
 var x = 5;
 var y = 6;
@@ -78,7 +78,7 @@ else
   m = y;
 return m;")
 
-(test-str #:id 8
+(test-str #:id "!= cond"
           10 "
 var x = 5;
 var y = 6;
@@ -86,7 +86,7 @@ if (x != y)
   x = 10;
 return x;")
 
-(test-str #:id 9
+(test-str #:id "== cond"
           5 "
 var x = 5;
 var y = 6;
@@ -94,20 +94,22 @@ if (x == y)
   x = 10;
 return x;")
 
-(test-str #:id 10
+(test-str #:id "negative sign on nested expression"
           -39 "return 6 * -(4 * 2) + 9;")
+
 
 ; ; Error Tests
 
 (error-str #:id "accessing var before declaring it"
            "return x;")
 
-(error-str #:id 11 "
+(error-str #:id "assign w/out declaring"
+           "
 var x = 1;
 y = 10 + x;
 return y;")
 
-(error-str #:id 12 "
+(error-str #:id "reference w/out declare or initializing" "
 var y;
 y = x;
 return y;")
@@ -115,24 +117,57 @@ return y;")
 (error-str #:id "accessing x before declaring it"
            "return x;")
 
-(error-str #:id 13 "
+(error-str #:id "reference w/out initializing"
+           "
 var x;
 var y;
 x = x + y;
 return x;")
 
-(error-file #:id 14 "
+(error-str #:id "redeclare in same scope"
+            "
 var x = 10;
 var y = 20;
 var x = x + y;
 return x;")
 
+(error-str #:id "var in block not accessible from subsequent outer scope"
+           "
+var x = 10;
+var y = 4;
+if (x < y) {
+  var min = x;
+}
+else {
+  var min = y;
+}
+return min;")
+
+
+(error-str #:id "var in block can't access undeclared var"
+           "
+if (true) {
+  a = 0;
+}
+return a;")
+
+(error-str #:id "var in block not accessible in later block (same depth in stack)"
+           "
+if (true) {
+  var a = 0;
+}
+if (true) {
+  a = 2;
+}
+return a;")
+
+
 ; ; Boolean, If, While Tests
 
-(test-str #:id 15
+(test-str #:id "return nested bool expr"
           'true "return (10 > 20) || (5 - 6 < 10) && true;")
 
-(test-str #:id 16
+(test-str #:id "return literal in if-else"
           100 "
 var x = 10;
 var y = 20;
@@ -168,7 +203,7 @@ if (false) {
 return x;
 ")
 
-(test-str #:id 17
+(test-str #:id "return var assigned in if-else"
           'false "
 var x = 100 % 2 == 0;
 var y = 10 >= 20;
@@ -179,7 +214,7 @@ else
   z = x;
 return z;")
 
-(test-str #:id 18
+(test-str #:id "return var assigned in if-else"
           'true "
 var x = 10;
 var y = 20;
@@ -190,7 +225,7 @@ else
   z = z;
 return z;")
 
-(test-str #:id 19
+(test-str #:id "update var in while loop, return"
           128 "
 var x = 2;
 while (x < 100)
@@ -205,7 +240,7 @@ while (x < 100) {
 }
 return x;")
 
-(test-str #:id 20
+(test-str #:id "update var in while loop, update after, return"
           12 "
 var x = 20;
 var y = 128;
@@ -235,14 +270,14 @@ return x;
 
 ; ; Advanced Tests
 
-(test-str #:id 21
+(test-str #:id "nested assign"
           30 "
 var x;
 var y;
 var z = x = y = 10;
 return x + y + z;")
 
-(test-str #:id 22
+(test-str #:id "assign in if cond"
           11 "
 var x;
 var y;
@@ -252,25 +287,25 @@ if ((x = x + 1) > y)
 else
   return y;")
 
-(test-str #:id 23
+(test-str #:id "assign x twice in same expr"
           1106 "
 var x;
 var y = (x = 5) + (x = 6);
 return y * 100 + x;")
 
-(test-str #:id 24
+(test-str #:id "reference x twice in expr, update in left should carry over to right operand"
           12 "
 var x = 10;
 x = (x = 6) + x;
 return x;")
 
-(test-str #:id 25
+(test-str #:id "update in right operand should not affect left operand"
           16 "
 var x = 10;
 x = x + (x = 6);
 return x;")
 
-(test-str #:id 26
+(test-str #:id "nested assign"
           72 "
 var x;
 var y;
@@ -278,14 +313,14 @@ var z;
 var w = (x = 6) + (y = z = 20);
 return w + x + y + z;")
 
-(test-str #:id 27
+(test-str #:id "assign in while cond"
           21 "
 var x = 0;
 while ((x = x + 1) < 21)
   x = x;
 return x;")
 
-(test-str #:id 28
+(test-str #:id "modulus, assign expr in left and right operands"
           164 "
 var a = 31160;
 var b = 1476;
@@ -293,4 +328,40 @@ var r = a % b;
 while (r != 0)
   r = (a = b) % (b = r);
 return b;")
+
+
+;;;; Static scoping
+
+
+(test-str #:id "var in block shadows outer scope var"
+          10 "
+var a = 10;
+if (true) {
+  var a = 2;
+}
+return a;")
+
+(test-str #:id "var in block can access outer scope var"
+          0 "
+var a = 10;
+if (true) {
+  a = 0;
+}
+return a;")
+
+(test-str #:id "nested blocks are consistent"
+          3 "
+var a = 0;
+if (true) {
+  var a = 1;
+  if (true) {
+    var a = 2;
+    if (true) {
+      var a = 3;
+      if (true) {
+        return a;
+      }
+    }
+  }
+}")
 
