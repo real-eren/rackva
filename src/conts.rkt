f500bb71dd16a7316df085afa961b790a9398e3c
real-eren <exk379@case.edu>
2022-04-15 13:13:18 -0400

    remove unused methods from conts

--- src/conts.rkt
+++ src/conts.rkt
@@ -4,15 +4,10 @@
 
 (provide (combine-out conts-of
                       return
-                      set-return
                       break
-                      set-break
                       continue
-                      set-continue
                       next
-                      set-next
-                      throw
-                      set-throw))
+                      throw))
 
 ;;;; Container of continuations used by the interpreter
 ;; these include return, next, break, continue, throw, catch, finally
@@ -29,18 +24,12 @@
            #:continue [con (continue conts)]
            #:next     [nxt (next conts)]
            #:throw    [thr (throw conts)])
-    (map:from-interlaced-entry-list
-     (list return-key   ret
-           break-key    brk
-           continue-key con
-           next-key     nxt
-           throw-key    thr
-           )
-     map:empty)))
-
-(define setter
-  (lambda (key)
-    (lambda (value conts) (map:put key value conts))))
+    (map:from-interlaced-entries
+     return-key   ret
+     break-key    brk
+     continue-key con
+     next-key     nxt
+     throw-key    thr)))
 
 (define getter
   (lambda (key)
@@ -49,20 +38,15 @@
 
 (define return-key 'return)
 (define return (getter return-key))
-(define set-return (setter return-key))
 
 (define break-key 'break)
 (define break (getter break-key))
-(define set-break (setter break-key))
 
 (define continue-key 'continue)
 (define continue (getter continue-key))
-(define set-continue (setter continue-key))
 
 (define next-key 'next)
 (define next (getter next-key))
-(define set-next (setter next-key))
 
 (define throw-key 'throw)
 (define throw (getter throw-key))
-(define set-throw (setter throw-key))
