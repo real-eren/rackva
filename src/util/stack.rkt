97aa1c1349b693819e649903707ba39d6ed7d281
real-eren <exk379@case.edu>
2022-04-19 23:48:33 -0400

    add stack abstraction, partially implement layered tables in state

---
+++ src/util/stack.rkt
@@ -0,0 +1,24 @@
+#lang racket
+;;;; Stack
+;; "abstraction" for a stack
+; a singly-linked list is already a stack,
+; so this module's purpose is primarily to provide
+; better names for common functions
+(provide (prefix-out stack: (combine-out (all-defined-out)
+                                         ormap
+                                         foldl)))
+
+(define push cons)
+(define peek car)
+(define pop cdr)
+
+(define update-front
+  (lambda (proc stack)
+    (push (proc (peek stack))
+          (pop stack))))
+
+(define of list)
+
+
+
+
