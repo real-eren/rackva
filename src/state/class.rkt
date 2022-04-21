6ad18bd4145061766f4eb5183fac6bd764848b56
real-eren <exk379@case.edu>
2022-04-21 10:32:04 -0400

    add `name` and `abstract-methods` properties

--- src/state/class.rkt
+++ src/state/class.rkt
@@ -1,29 +1,34 @@
 #lang racket
 
 (require "../util/map.rkt")
+(provide (prefix-out class: (all-defined-out)))
 
 ;;;; Class
-;; super-class
-;; constructors
 
 (define of
-  (lambda (#:parent parent-name
+  (lambda (#:name name
+           #:parent parent-name
            #:ctors constructors
            #:i-fields instance-fields
            #:i-methods instance-methods
            #:s-fields static-fields
-           #:s-methods static-methods)
+           #:s-methods static-methods
+           #:a-methods abstract-methods)
     (map:of
+     $name           name
      $super-class    parent-name
      $constructors   constructors
      $i-field-inits  instance-fields
      $i-methods      instance-methods
      $s-fields       static-fields
-     $s-methods      static-methods)))
+     $s-methods      static-methods
+     $a-methods      abstract-methods)))
 
+(define $name 'name)
 (define $super-class 'super-class)
 (define $constructors 'constructors)
 (define $i-field-inits 'i-field-inits)
 (define $i-methods 'i-methods)
 (define $s-fields 's-fields)
 (define $s-methods 's-methods)
+(define $a-methods 'a-methods)
