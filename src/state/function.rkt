2ebea34fdec84394a375e79e5a826f61e4f00e71
real-eren <exk379@case.edu>
2022-04-21 10:37:53 -0400

    extract function from function-table, add class property, add initial unit tests

--- src/state/function.rkt
+++ src/state/function.rkt
@@ -2,18 +2,7 @@
 
 (require "../util/map.rkt")
 
-(provide (prefix-out function:
-                     (combine-out $params
-                                  $body
-                                  $scoper
-                                  $class
-                                  $type
-                                  type:static
-                                  type:instance
-                                  type:free
-                                  type:abstract
-                                  num-formal-params
-                                  )))
+(provide (prefix-out function: (all-defined-out)))
 
 ;;;; function property keys
 (define $name   'fun-name)
@@ -24,11 +13,12 @@
 ;; state mapping function that filters the layers that should be visible during execution
 (define $scoper 'scoper)
 ; what class this function is defined in, if applicable.
-; Null if type == free
+; null or absent if type == free
 (define $class  'class)
 ;; what kind of function is this
-; constants for Static | Instance | Free | Abstract
 (define $type   'type)
+(define type (map:getter $type))
+; constants for Static | Instance | Free | Abstract
 (define type:static   'static)
 (define type:instance 'instance)
 (define type:free     'free)
@@ -36,13 +26,19 @@
 
 
 (define of
-  (lambda (name params body scoper type)
+  (lambda (#:name name
+           #:params params
+           #:body body
+           #:scoper scoper
+           #:type type
+           #:class class)
     (map:of
      $name    name
      $params  params
      $body    body
      $scoper  scoper
-     $type    type)))
+     $type    type
+     $class   class)))
 
 (define of-abstract
   (lambda (name params)
@@ -53,5 +49,6 @@
 ;; returns the number of formal parameters in a function
 (define num-formal-params
   (lambda (closure)
+    ; ignore ampersands, they mark the following symbol as by-reference
     (length (filter (lambda (p) (not (eq? '& p))) (map:get $params closure)))))
 
