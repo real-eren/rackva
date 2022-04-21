b405c3e0d5cf98afaba2820aaab8e8f1b0fd021c
real-eren <exk379@case.edu>
2022-04-21 17:48:08 -0400

    extract common unit test logic, split error tests from normal tests

--- src/v1-tests.rkt
+++ src/v1-tests.rkt
@@ -1,33 +1,14 @@
 #lang racket/base
 
-(require rackunit 
-         "interpreter.rkt"
-         "interpreter-extension.rkt"
+(require "interpreter-extension.rkt"
          "util/testing.rkt")
 
 ; white space / newlines do not affect the parser,
 ; but are included for readability
 
 
-(define test-file
-  (lambda (expected file #:id [test-id #f])
-    (check-equal? (interpret-v1-file file) expected (format-test-id test-id))))
-
-(define test-str
-  (lambda (expected str #:id [test-id #f])
-    (check-equal? (interpret-v1-str str) expected (format-test-id test-id))))
-
-(define error-file
-  (lambda (file #:id [test-id #f] #:catch [suppress #t])
-    (if suppress
-        (check-exn exn:fail:user? (lambda () (interpret-v1-file file)) (format-test-id test-id))
-        (test-file 'error file #:id test-id))))
-
-(define error-str
-  (lambda (str #:id [test-id #f] #:catch [suppress #t])
-    (if suppress
-        (check-exn exn:fail:user? (lambda () (interpret-v1-str str)) (format-test-id test-id))
-        (test-str 'error str #:id test-id))))
+(define test-file (make-tester interpret-v1-file))
+(define test-str (make-tester interpret-v1-str))
 
 ; ; Literal Tests
 (test-str #:id "return a positive literal"
@@ -115,102 +96,6 @@ return x;
 x = x + 25;
 return x;")
 
-; ; Error Tests
-
-(error-str #:id "accessing var before declaring it"
-           #:catch #t
-           "return x;")
-
-(error-str #:id "assign w/out declaring"
-           #:catch #t
-           "
-var x = 1;
-y = 10 + x;
-return y;")
-
-(error-str #:id "reference w/out declare or initializing"
-           #:catch #t
-           "
-var y;
-y = x;
-return y;")
-
-
-(error-str #:id "reference w/out initializing"
-           #:catch #t
-           "
-var x;
-var y;
-x = x + y;
-return x;")
-
-(error-str #:id "redeclare in same scope"
-           #:catch #t
-           "
-var x = 10;
-var y = 20;
-var x = x + y;
-return x;")
-
-(error-str #:id "var in block not accessible from subsequent outer scope"
-           #:catch #t
-           "
-var x = 10;
-var y = 4;
-if (x < y) {
-  var min = x;
-}
-else {
-  var min = y;
-}
-return min;")
-
-
-(error-str #:id "var in block can't access undeclared var"
-           #:catch #t
-           "
-if (true) {
-  a = 0;
-}
-return a;")
-
-(error-str #:id "var in block not accessible in later block (same depth in stack)"
-           #:catch #t
-           "
-if (true) {
-  var a = 0;
-}
-if (true) {
-  a = 2;
-}
-return a;")
-
-(error-str #:id "var declared in first iter of while loop doesn't persist through later iters)"
-           #:catch #t
-           "
-var x = 0;
-while (x < 5) {
-  if (x != 0) {
-    return a;
-  }
-  var a = 5;
-  x = x + 1;
-}
-return 0;")
-
-(error-str #:id "break outside of loop"
-           #:catch #t
-           "
-var x = 1;
-break;
-return x;")
-
-(error-str #:id "continue outside of loop"
-           #:catch #t
-           "
-var x = 1;
-continue;
-return x;")
 
 ; ; Boolean, If, While Tests
 
@@ -747,91 +632,6 @@ finally {
 }
 return x;
 ")
-
-(error-str #:id "Top-level throw"
-           #:catch #t
-           "
-throw 1;
-")
-
-(error-str #:id "Uncaught throw in (entered) catch block"
-           #:catch #t
-           "
-var x = 10;
-var result = 1;
-try {
-  while (x < 10000) {
-    result = result - 1;
-    x = x * 10;
-    if (x > 1000)
-      throw x;
-  }
-}
-catch (ex) {
-  throw 1;
-}
-return result;")
-
-(error-str #:id "break exit from block pops frame"
-           #:catch #t
-           "
-var x = 0;
-while (true) {
-  x = x + 1;
-  var a = 5;
-  if (x == 2) {
-    break;
-  }
-}
-return a;
-")
-
-(error-str #:id "continue in while body block, pop frame"
-           #:catch #t
-           "
-var x = 1;
-var y = 2;
-if (x < y) {
-  var z = 0;
-  while (z < 100) {
-    var a = 1;
-    z = z + a;
-    continue;
-    z = 1000;
-  }
-  if (z != x) {
-    z = a;
-  }
-}
-return x;
-")
-
-(error-str #:id "throw in block, should pop frame. var from try not visible in catch"
-           #:catch #t
-           "
-try {
-  var a = 1;
-  throw 0;
-}
-catch (e) {
-  return a;
-}
-")
-
-(error-str #:id "throw in block, should pop frame. var from try not visible in finally"
-           #:catch #t
-           "
-try {
-  var a = 1;
-  throw 0;
-}
-catch (e) {
-}
-finally {
-  return a;
-}
-")
-
 
 (test-str #:id "while loop with continue inside try and try is inside while"
           22 "
---
+++ src/v2-error-tests.rkt
@@ -0,0 +1,210 @@
+#lang racket/base
+
+(require "interpreter-extension.rkt"
+         "util/testing.rkt")
+
+
+(define error-file (make-error-tester interpret-v2-file))
+(define error-str (make-error-tester interpret-v2-str))
+
+
+; ; Error Tests
+
+(error-str #:id "invoking undefined function in top level var declaration"
+           #:catch #t
+           "
+var x = f();
+
+function f() { return 2; }
+
+function main() {
+  return x;
+}")
+
+(error-str #:id "invoking nested function before its definition in the same scope"
+           #:catch #t
+           "
+function main() {
+  a = nested();
+  function nested() { return 1; }
+  return a;
+}")
+
+(error-str #:id "Passing too many arguments"
+           #:catch #t
+           "
+function f(a) { return a*a; }
+
+function main() {
+  return f(10, 11, 12);
+}")
+
+(error-str #:id "Passing too few arguments"
+           #:catch #t
+           "
+function f(a, b, c) { return a + b + c; }
+function main() {
+  return f(1);
+}")
+
+(error-str #:id "Passing expressions to a reference parameter"
+           #:catch #t
+           "
+function f(&a) { a = a + 1; }
+function main() {
+  return f(1);
+}")
+
+(error-str #:id "Functions inside functions accessing out of scope variables."
+           #:catch #t
+           "
+function f(x) {
+  function g(x) {
+    var b;
+    b = x;
+    return 0;
+  }
+
+  function h(x) {
+    b = x;
+    return 1;
+  }
+
+  return g(x) + h(x);
+}
+
+function main() {
+  return f(10);
+}")
+
+(error-str #:id "global function defined twice"
+           #:catch #t
+           "
+function foo() { return 1; }
+function foo() { return 2; }
+function main() {
+  var x = foo();
+  return x;
+}")
+
+(error-str #:id "absent main function"
+           #:catch #t
+           "var a = 2;")
+
+(error-str #:id "main without return"
+           #:catch #t
+           "
+function main() {
+  var a = 2;
+}
+")
+
+(error-str #:id "throw in main"
+           #:catch #t
+           "
+function main() {
+  throw 1;
+}")
+
+(error-str #:id "break in main w/out while"
+           #:catch #t
+           "
+function main() {
+  break;
+}")
+
+(error-str #:id "break in function w/out while"
+           #:catch #t
+           "
+function f() { break; }
+function main() {
+  return f();
+}")
+
+(error-str #:id "continue in main w/out while"
+           #:catch #t
+           "
+function main() {
+  continue;
+}")
+
+(error-str #:id "continue in function w/out while"
+           #:catch #t
+           "
+function f() { continue; }
+function main() {
+  return f();
+}")
+
+(error-str #:id "global var initializer invokes later global function"
+           #:catch #t
+           "
+var x = f();
+function f() { return 5; }
+function main() {
+  return x;
+}")
+
+(error-str #:id "nested fun refers to 'nephew' nested fun"
+           #:catch #t
+           "
+function main() {
+  function bar() { return nephew(); }
+  function sibling() {
+    function newphew() { return 5; }
+  }
+  return bar();
+}")
+
+(error-str #:id "nested fun refers to 'cousin' nested fun. Same height different branches"
+           #:catch #t
+           "
+function main() {
+  function a1() {
+    function a2() {
+      function a3() { return b2(); }
+      return a3();
+    }
+    return a2();
+  }
+  function b1() {
+    function b2() { return 5; }
+    function b2b() { return a1(); }
+    return b2b();
+  }
+  return b1();
+}")
+
+; not specified whether params can be shadowed by locals
+(error-str #:id "local name collides with param name"
+           #:catch #t
+           "
+function foo(x) {
+  var x = 2;
+  return x;
+}
+function main() {
+  return foo(5);
+}")
+
+(error-str #:id "reading from reference to uninitialized variable"
+           #:catch #t
+           "
+function foo(&a) { return a; }
+function main() {
+  var x;
+  return foo(x);
+}
+")
+
+(error-str #:id "function w/out return used as expression"
+           #:catch #t
+           "
+function noReturn() {
+  var x;
+  x = 2;
+}
+
+function main() {
+  return noReturn();
+}")
