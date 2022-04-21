320af5744a19f90f973e7e522d99dae0630c1c8b
real-eren <exk379@case.edu>
2022-04-21 10:37:53 -0400

    extract function from function-table, add class property, add initial unit tests

--- src/state/function-table.rkt
+++ src/state/function-table.rkt
@@ -10,68 +10,52 @@
                                   get
                                   declare-fun)))
 
-
 ;;;; function table
 ;; a function table a map of function bindings
 ;; bindings { name : function }
 
-;;;; function property keys
-(define $name   'fun-name)
-;; list of formal parameters
-(define $params 'formal-params)
-;; statement list that forms the body of the function
-(define $body   'body)
-;; state mapping function that filters the layers that should be visible during execution
-(define $scoper 'scoper)
-; what class this function is defined in, if applicable.
-; Null if type == free
-(define $class  'class)
-;; what kind of function is this
-; constants for Static | Instance | Free | Abstract
-(define $type   'type)
-(define type:static   'static)
-(define type:instance 'instance)
-(define type:free     'free)
-(define type:abstract 'abstract)
-
-
-(define function:of
-  (lambda (name params body scoper type)
-    (map:of
-     $name    name
-     $params  params
-     $body    body
-     $scoper  scoper
-     $type    type)))
-
-;; returns the number of formal parameters in a function
-(define num-formal-params
-  (lambda (closure)
-    (length (filter (lambda (p) (not (eq? '& p))) (map:get $params closure)))))
-
-
 (define new-function-table map:empty)
 
+; get all functions with this name in the table
 (define get-all map:get-all)
-; gets the function with a matching signature
+;; gets the function with a matching signature
+; false if absent
 (define get
   (lambda (name arg-list table)
     (findf (lambda (f)
              (eq? (length arg-list)
-                  (num-formal-params f)))
+                  (function:num-formal-params f)))
            (get-all name table))))
 
 ; get returns false if absent
 (define has-fun?
   (lambda (name arg-list table)
-    (map:contains? name table)))
+    (not (eq? #f (get name arg-list table)))))
+
 
 (define declare-fun
-  (lambda (name params body scoper type table)
+  (lambda (name params body scoper type class table)
     (map:insert name
-                (function:of name
-                             params
-                             body
-                             scoper
-                             type)
+                (function:of #:name name
+                             #:params params
+                             #:body body
+                             #:scoper scoper
+                             #:type type
+                             #:class class)
                 table)))
+
+
+(module+ test
+  (require rackunit)
+  (check-false (has-fun? 'a '() new-function-table))
+  (check-eq? '() (get-all 'a new-function-table))
+
+  
+  (let* ([fname    'a]
+         [fparams  '(a b & c)]
+         [fargs    '(1 2 var)]
+         [table    (declare-fun fname fparams null null null null new-function-table)])
+    (check-true (has-fun? fname fargs table))
+    (check-false (has-fun? fname '() table))
+    (check-eq? (get fname fargs table) (first (get-all fname table)))))
+
