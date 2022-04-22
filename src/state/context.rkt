0151c72cc6f2f2c3cd799ca7a9467723fd1c5c8f
real-eren <exk379@case.edu>
2022-04-21 22:05:39 -0400

    add type `class-def-member` to context

--- src/state/context.rkt
+++ src/state/context.rkt
@@ -7,26 +7,44 @@
 ;; A context is a map of information about the current context of the interpreter
 ; i.e. is it currently executing a function call, declaring a class, etc
 
+(define of map:of)
+
 (define $type 'type)
 (define type (map:getter $type))
 
-(define of map:of)
-
-;; types of contexts:
+;;;; types of contexts:
 ; calling a function
 (define type:fun-call 'fun-call)
 ; defining a class
 (define type:class-def 'class-def)
+; defining a class member. previous layer in a context-stack should be a `type:class-def`
+(define type:class-def-member 'class-def-member)
 ; global statements, like var decl and fun decl
 (define type:top-level 'top-level)
 
-; used to handle dots
+; (undecided) used to handle dots
 (define type:scope 'scope)
 
-; type specific properties
+;;;; type specific properties
+; closure of function being called
 (define fun-call:$fun 'fun)
+(define fun-call:fun (map:getter fun-call:$fun))
 
+; name of class being defined 
 (define class-def:$name 'class-name)
+(define class-def:name (map:getter class-def:$name))
+
+; Where does this member belong. static | instance | abstract
+(define class-def-member:$scope 'member-scope)
+(define class-def-member:scope (map:getter class-def-member:$scope))
+(define class-def-member:scope:static 'static)
+(define class-def-member:scope:instance 'instance)
+(define class-def-member:scope:abstract 'abstract)
+
+; what kind of member is this. method | field ; may not need this
+(define class-def-member:$kind 'member-kind)
+(define class-def-member:kind:method 'method)
+(define class-def-member:kind:field 'field)
 
 ;; constructors for the various kinds of contexts
 (define of-fun-call
@@ -38,6 +56,12 @@
   (lambda (class-name)
     (of $type  type:class-def
         class-def:$name  class-name)))
+
+(define of-class-member-def
+  (lambda (scope kind)
+    (of $type  type:class-def-member
+        class-def-member:$scope  scope
+        class-def-member:$kind  kind)))
 
 (define top-level (of $type  type:top-level))
 
