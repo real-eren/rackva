002cb9fb255aedb8de9b9507ea15e69e1c89f70e
real-eren <exk379@case.edu>
2022-04-23 16:14:28 -0400

    simplify contexts. state provides specific functions for each kind of context. remove overly specific contexts

--- src/state/context.rkt
+++ src/state/context.rkt
@@ -18,8 +18,6 @@
 (define type:fun-call 'fun-call)
 ; defining a class
 (define type:class-def 'class-def)
-; defining a class member. previous layer in a context-stack should be a `type:class-def`
-(define type:class-def-member 'class-def-member)
 ; global statements, like var decl and fun decl
 (define type:top-level 'top-level)
 
@@ -35,19 +33,6 @@
 (define class-def:$name 'class-name)
 (define class-def:name (map:getter class-def:$name))
 
-; Where does this member belong. static | instance | abstract
-(define class-def-member:$scope 'member-scope)
-(define class-def-member:scope (map:getter class-def-member:$scope))
-(define class-def-member:scope:static 'static)
-(define class-def-member:scope:instance 'instance)
-(define class-def-member:scope:abstract 'abstract)
-
-; what kind of member is this. method | field ; may not need this
-(define class-def-member:$kind 'member-kind)
-(define class-def-member:kind (map:getter class-def-member:$kind))
-(define class-def-member:kind:method 'method)
-(define class-def-member:kind:field 'field)
-
 ;; constructors for the various kinds of contexts
 (define of-fun-call
   (lambda (fun-closure)
@@ -58,28 +43,6 @@
   (lambda (class-name)
     (of $type  type:class-def
         class-def:$name  class-name)))
-
-(define of-class-member-def
-  (lambda (scope kind)
-    (of $type  type:class-def-member
-        class-def-member:$scope  scope
-        class-def-member:$kind  kind)))
-
-
-(define static-field-decl (of-class-member-def class-def-member:scope:static
-                                               class-def-member:kind:field))
-
-(define instance-field-decl (of-class-member-def class-def-member:scope:instance
-                                                 class-def-member:kind:field))
-
-(define static-method-decl (of-class-member-def class-def-member:scope:static
-                                                class-def-member:kind:method))
-
-(define instance-method-decl (of-class-member-def class-def-member:scope:instance
-                                                  class-def-member:kind:method))
-
-(define abstract-method-decl (of-class-member-def class-def-member:scope:abstract
-                                                  class-def-member:kind:method))
 
 (define top-level (of $type  type:top-level))
 
