036c4f93baf97dc8b5f0033334c6b19e449f545c
real-eren <exk379@case.edu>
2022-04-23 14:40:09 -0400

    `state` - add `declare-method` to API, remoe method case from `declare-function`, change `declare-class` to just insert an empty class closure

--- src/state/class.rkt
+++ src/state/class.rkt
@@ -9,20 +9,17 @@
 
 (define of
   (lambda (#:name name
-           #:parent parent-name
-           #:ctors constructors
-           #:i-fields instance-fields)
+           #:parent parent-name)
     (map:of
      $name           name
      $super-class    parent-name
-     $constructors   constructors
-     $i-field-inits  instance-fields
+     $constructors   new-function-table
      $methods        new-function-table
      $s-fields       new-var-table)))
 
 (define $name 'name)
 (define $super-class 'super-class)
+(define $init 'init)
 (define $constructors 'constructors)
-(define $i-field-inits 'i-field-inits)
 (define $methods 'methods)
 (define $s-fields 's-fields)
