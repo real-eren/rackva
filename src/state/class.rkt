3a3640b3b6df1b4cefc50962a8071816ae620542
real-eren <exk379@case.edu>
2022-04-23 09:28:06 -0400

    merge method tables in `class`, change `class` constructor to create an empty class closure (to be filled in while evaluating class body), and remove unnecessary params from `state`::`declare-method`

--- src/state/class.rkt
+++ src/state/class.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(require "../util/map.rkt")
+(require "../util/map.rkt"
+         "function-table.rkt"
+         "var-table.rkt")
 (provide (prefix-out class: (all-defined-out)))
 
 ;;;; Class
@@ -9,26 +11,18 @@
   (lambda (#:name name
            #:parent parent-name
            #:ctors constructors
-           #:i-fields instance-fields
-           #:i-methods instance-methods
-           #:s-fields static-fields
-           #:s-methods static-methods
-           #:a-methods abstract-methods)
+           #:i-fields instance-fields)
     (map:of
      $name           name
      $super-class    parent-name
      $constructors   constructors
      $i-field-inits  instance-fields
-     $i-methods      instance-methods
-     $s-fields       static-fields
-     $s-methods      static-methods
-     $a-methods      abstract-methods)))
+     $methods        new-function-table
+     $s-fields       new-var-table)))
 
 (define $name 'name)
 (define $super-class 'super-class)
 (define $constructors 'constructors)
 (define $i-field-inits 'i-field-inits)
-(define $i-methods 'i-methods)
+(define $methods 'methods)
 (define $s-fields 's-fields)
-(define $s-methods 's-methods)
-(define $a-methods 'a-methods)
