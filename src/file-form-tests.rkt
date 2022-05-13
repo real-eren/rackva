3317d9c8674bd402c949a5ce9f5b398dac93a7fc
real-eren <exk379@case.edu>
2022-05-12 20:59:34 -0400

    change `file-form-tests.rkt` to use `interpret` function

--- src/file-form-tests.rkt
+++ src/file-form-tests.rkt
@@ -1,10 +1,11 @@
 #lang racket/base
 (require "util/testing.rkt"
-         "interpreter-extension.rkt")
+         "interpreter-extension.rkt"
+         "interpreter.rkt")
 
 (define test-file-v1 (make-tester interpret-v1-file))
 (define test-file-v2 (make-tester interpret-v2-file))
-(define test-file-v3 (make-tester interpret-v3-file))
+(define test-file-v3 (make-tester interpret))
 
 ;;;; tiny subset of tests to check that interpret handles file input correctly
 (test-file-v1 11 "file-tests/v1/1.txt")
