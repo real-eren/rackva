a9e44263f60a6dbcdd24765bc7e4a8e61982cf7e
real-eren <exk379@case.edu>
2022-05-31 00:27:45 -0400

    parser-tests: add expected AST values, remove exn tests

--- test/parse/parser-tests.rkt
+++ test/parse/parser-tests.rkt
@@ -4,9 +4,9 @@
 
 ; ; v1
 
-(test-not-exn
+(test-equal?
  "v1, comprehensive"
- (λ () (parse-str "
+ (parse-str "
 var x = 0;
 while(x < 12) {
   try{
@@ -19,13 +19,22 @@ while(x < 12) {
   }
 }
 return x;
-")))
+")
+ '((var x 0)
+   (while
+    (< x 12)
+    (begin
+      (try
+       ((= x (+ x 10)) (break))
+       (catch (e) ((if (|| true false) (return 0))))
+       (finally ((= x (+ x 1)))))))
+   (return x)))
 
 ; ; v2
 
-(test-not-exn
+(test-equal?
  "v2, comprehensive"
- (λ () (parse-str "
+ (parse-str "
 function divide(x, y) {
   if (y == 0)
     throw 1000000;
@@ -44,7 +53,7 @@ function divide(x, y) {
         x = x + divide(10*i, i);
       }
       catch(e) {
-        x = x + divide(e, j);
+        x = x + divide(e, j,);
       }
       i = i - 1;
     }
@@ -55,18 +64,41 @@ function divide(x, y) {
     x = x * 2;
   }
   return x;
-}")))
+}")
+ '((function divide (x y) ((if (== y 0) (throw 1000000)) (return (/ x y))))
+   (function
+    main
+    ()
+    ((var x 0)
+     (var j 1)
+     (try
+      ((while
+        (>= j 0)
+        (begin
+          (var i 10)
+          (while
+           (>= i 0)
+           (begin
+             (try
+              ((= x (+ x (funcall divide (* 10 i) i))))
+              (catch (e) ((= x (+ x (funcall divide e j)))))
+              ())
+             (= i (- i 1))))
+          (= j (- j 1)))))
+      (catch (e2) ((= x (* x 2))))
+      ())
+     (return x)))))
 
 ; ; v3
 
-(test-not-exn
+(test-equal?
  "v3, comprehensive"
- (λ () (parse-str "
+ (parse-str "
 class A extends Bclass {
   static var sfield;
   var ifield = c;
   var ifield = 1 + x.y.funcall();
-  A() { super(); }
+  A() { super(); x = 3; }
   A(a, b) { this(); }
   function abstr(&a, b);
   function inst(a, b, &c) {
@@ -78,13 +110,28 @@ class A extends Bclass {
     a();
     throw 3 + 3;
   }
-}")))
+}")
+ '((class A
+     (extends Bclass)
+     ((static-var sfield)
+      (var ifield c)
+      (var ifield (+ 1 (funcall (dot (dot x y) funcall))))
+      (constructor () ((funcall super) (= x 3)))
+      (constructor (a b) ((funcall this)))
+      (abstract-function abstr (& a b))
+      (function
+       inst
+       (a b & c)
+       ((funcall (dot this x))
+        (funcall (dot super foo) (dot this y))
+        (return (dot super x))))
+      (static-function main () ((funcall a) (throw (+ 3 3))))))))
 
 ; ; Mixture
 
-(test-not-exn
+(test-equal?
  "v1+v2+v3, comprehensive"
- (λ () (parse-str "
+ (parse-str "
 var a = 3;
 function foo() { return 3; }
 
@@ -98,58 +145,14 @@ throw 0;
 try {} finally {}
 if (false) {}
 
-class A { }")))
-
-
-; ; Illegal usages that should be caught
-(define-check (test-parser-exn msg prog-str)
-  (test-exn msg exn:fail:user? (λ () (parse-str prog-str))))
-
-(test-case
- "control flow in class body"
- (test-parser-exn
-  "`if` in class body"
-  "class A { if (true) { } }")
- (test-parser-exn
-  "`while` in class body"
-  "class A { while (true) { } }")
- (test-parser-exn
-  "`try` in class body"
-  "class A { try { } catch {} }")
- (test-parser-exn
-  "return in class body"
-  "class A { return 0; }"))
-
-(test-case
- "class in nested statement list"
- (test-parser-exn
-  "class in `if`"
-  "if (true) { class A {} }")
- (test-parser-exn
-  "class in `while`"
-  "while (true) { class A {} }")
- (test-parser-exn
-  "class in `block`"
-  "{ class A {} }")
- (test-parser-exn
-  "class in class"
-  "class AA { class A {} }")
- (test-parser-exn
-  "class in function"
-  "function foo() { class A {} }"))
-
-(test-case
- "class members outside class"
- (test-parser-exn
-  "static var in global"
-  "static var a;")
- (test-parser-exn
-  "static fun in global"
-  "static function a(){ }")
- (test-parser-exn
-  "abstract function in global"
-  "function a();")
- (test-parser-exn
-  "abstract function in block"
-  "{ function a(); }"))
+class A { }")
+ '((var a 3)
+   (function foo () ((return 3)))
+   (while false (begin (= a (+ a (funcall foo)))))
+   (class B () ())
+   (return false)
+   (throw 0)
+   (try () () (finally ()))
+   (if false (begin))
+   (class A () ())))
 
